4a0b461702776d9b6e14b49d7d33c52106dc7391
novilfahlevy <novilfreon@gmail.com>
2026-01-10 15:52:13 +0800

    remove notif

--- resources/views/layouts/app-header.blade.php
+++ resources/views/layouts/app-header.blade.php
@@ -107,7 +107,7 @@
                 </button>
 
                 <!-- Notification Dropdown -->
-                <x-header.notification-dropdown />
+                {{-- <x-header.notification-dropdown /> --}}
             </div>
 
             <!-- User Dropdown -->
